85752ed45ae9dac0077be58fd478ae0dbafdfcb6
JorgeDev7 <jorgehumbertocolindres@gmail.com>
2023-04-28 15:20:14 -0600

    Estructuracion de la aplicacion finalizada

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,7 +1,25 @@
 import Head from "next/head"
 
-export default function Layout({ pagina }) {
+export default function Layout({ children, pagina }) {
     return (
-        <div>Layout</div>
+        <>
+            <Head>
+                <link rel="icon" type="image/png" href="/assets/images/bk-logo.png" />
+                <meta name="application-name" content="Burger King" />
+                <title>Burger King - {pagina}</title>
+            </Head>
+
+            <header>
+                {/* navegacion */}
+            </header>
+
+            <main>
+                {children}
+            </main>
+
+            <footer>
+
+            </footer>
+        </>
     )
 }
