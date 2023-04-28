cd9e4edfef94d2c3f8e302140c54d79605d1ffd1
JorgeDev7 <jorgehumbertocolindres@gmail.com>
2023-04-28 10:52:17 -0600

    Colores personalizados añadidos

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,18 @@ module.exports = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/layout/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      colors: {
+        'fondo': '#f1ede4',
+        'amarillo': '#fda736;',
+        'rojo': '#e02222',
+        'grisOscuro': '#3a3a3a',
+        'marron': '#905432',
+        'naranja': '#e8512e'
+      }
     },
   },
   plugins: [],
