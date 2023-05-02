2ad15df9893d879607fc057629c620f32ebd29d9
JorgeDev7 <jorgehumbertocolindres@gmail.com>
2023-05-02 15:23:37 -0600

    Contenido principal: Heading, imagen y seccion de horario creado

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,6 @@
 import Image from 'next/image'
-import { Sigmar_One, Roboto } from 'next/font/google'
+import { Roboto } from 'next/font/google'
 import Layout from '@/layout/Layout';
-
-const sigmar = Sigmar_One({
-  weight: '400',
-  style: 'normal',
-  subsets: ['latin']
-});
 
 const roboto = Roboto({
   weight: '400',
@@ -15,7 +9,24 @@ const roboto = Roboto({
 })
 
 export default function Home() {
+
   return (
-    <Layout></Layout>
+    <Layout
+      pagina="Home"
+    >
+      <h1>Es hora del <span>desayuno</span></h1>
+      <Image
+        width={600}
+        height={700}
+        src="/assets/images/bk-desayuno.png"
+        alt="logo BK"
+        priority={true}
+      />
+
+      <div>
+        <h2>Ordena a domicilio de</h2>
+        <p>7:00 am a 22:00 pm</p>
+      </div>
+    </Layout>
   )
 }
