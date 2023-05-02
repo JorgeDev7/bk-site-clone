9d69a1cb5a75fae2100ad9eb9bd43acdd773789e
JorgeDev7 <jorgehumbertocolindres@gmail.com>
2023-05-02 17:36:58 -0600

    Fuente Caveat eliminada, sustituida por DANCING_SCRIPT, instancias de fuentes modificadas, eliminando sus parametros 'weight' y 'style', parametro 'variable añadido con su respectivo nombre'

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,20 @@
 import Image from 'next/image'
-import { Roboto, Caveat, Montserrat } from 'next/font/google'
+import { Roboto, Montserrat, Dancing_Script } from 'next/font/google'
 import Layout from '@/layout/Layout';
 
 const roboto = Roboto({
-  weight: '400',
-  style: 'normal',
-  subsets: ['latin']
+  subsets: ['latin'],
+  variable: '--font-roboto'
 })
 
-const caveat = Caveat({
-  weight: '500',
-  style: 'normal',
-  subsets: ['latin']
+const dancingScript = Dancing_Script({
+  subsets: ['latin'],
+  variable: '--font-dancing'
 })
 
 const montserrat = Montserrat({
-  weight: '900',
-  style: 'normal',
-  subsets: ['latin']
+  subsets: ['latin'],
+  variable: '--font-mont'
 })
 
 export default function Home() {
@@ -26,17 +23,18 @@ export default function Home() {
     <Layout
       pagina="Home"
     >
-      <h1 className='text-amarillo'>Es hora del <span>desayuno</span></h1>
+      <h1 className={`text-amarillo ${montserrat} font-black text-5xl md:text-7xl lg:text-8xl uppercase text-end mt-5`}>Es hora del <span className='text-rojo block'>desayuno</span></h1>
       <Image
-        width={600}
-        height={700}
+        width={750}
+        height={850}
         src="/assets/images/bk-desayuno.png"
         alt="logo BK"
+        className='mx-auto -mt-5 lg:-mt-20'
         priority={true}
       />
 
-      <div>
-        <h2>Ordena a domicilio de</h2>
+      <div className='border-y-2 border-y-grisOscuro border-dashed px-10 w-max mx-auto'>
+        <h2 className={`${dancingScript.variable} font-medium text-grisOscuro `}>Ordena a domicilio de</h2>
         <p>7:00 am a 22:00 pm</p>
       </div>
     </Layout>
