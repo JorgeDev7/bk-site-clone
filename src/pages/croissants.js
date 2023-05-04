89cad826dbb4f3fbdd7d39e9dacaa13ed2468019
JorgeDev7 <jorgehumbertocolindres@gmail.com>
2023-05-04 12:17:21 -0600

    useState, useEffect y dependencia axios importados, State para almacenar los productos creado, funcion que consulta la api creada, useEffect creado para llamar a la funcion previa

--- src/pages/croissants.js
+++ src/pages/croissants.js
@@ -1,6 +1,22 @@
+import { useState, useEffect } from "react"
 import Layout from "@/layout/Layout"
+import axios from "axios"
 
 export default function Croissants() {
+
+    const [croissants, setCroissants] = useState([]);
+
+    const obtenerCroissant = async () => {
+        const { data } = await axios('http://localhost:4000/productos?producto.categoria=croissants');
+
+        setCroissants(data);
+    }
+
+    useEffect(() => {
+        obtenerCroissant();
+    }, [])
+
+
     return (
         <Layout
             pagina="Croissants"
