7de9156462be20901b1b37bd13a1e65cd8d20057
JorgeDev7 <jorgehumbertocolindres@gmail.com>
2023-05-08 10:47:02 -0600

    Hook personalizado creado

--- hooks/useBk.jsx
+++ hooks/useBk.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import BkContext from "../context/bkProvider";
+import BkContext from "../context/BkProvider";
 
 const useBk = () => {
     return useContext(BkContext);
