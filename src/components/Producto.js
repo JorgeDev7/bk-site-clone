18182c5bdefd30437b89d12703e1a56c502def91
JorgeDev7 <jorgehumbertocolindres@gmail.com>
2023-05-08 12:08:51 -0600

    Componente creado para contener imagen de los productos, Image importado y router para redireccionamiento al endpoint dinamico

---
+++ src/components/Producto.js
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import { useRouter } from "next/router";
+
+export default function Producto({ datos }) {
+
+    const { imagen, nombre, id } = datos;
+
+    const router = useRouter();
+
+    return (
+        <Image
+            key={datos.id}
+            width={300}
+            height={400}
+            src={`/assets/images/Croissants/${imagen}.jpg`}
+            alt={`imagen croissant ${nombre}`}
+            onClick={() => router.push(`/producto/${id}`)}
+            priority={true}
+        />
+    )
+}
