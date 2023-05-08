a54a444935b80a453bb106445cb4514f871aa7ea
JorgeDev7 <jorgehumbertocolindres@gmail.com>
2023-05-08 12:07:58 -0600

    Layout y axios importados, funcion getServerSideProps creada, como argumento toma el query con el id de la url dinamica, se realiza llamado a la api y se colocan los datos para consumirlos como prop

--- src/pages/producto/[id].js
+++ src/pages/producto/[id].js
@@ -1,5 +1,25 @@
-export default function croissant() {
+import Layout from "@/layout/Layout"
+import axios from "axios"
+
+export default function croissant({ data }) {
+
+    const { descripcion, imagen, precio, nombre } = data[0].producto;
+
     return (
-        <div>croissant</div>
+        <Layout
+            pagina={nombre}
+        ></Layout>
     )
+}
+
+
+export async function getServerSideProps({ query: { id } }) {
+
+    const { data } = await axios(`http://localhost:4000/productos?producto.id=${id}`);
+
+    return {
+        props: {
+            data
+        }
+    }
 }
