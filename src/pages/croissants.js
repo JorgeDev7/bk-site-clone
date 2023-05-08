eb7172cd5c7426a6121193204a9509f1b516c78c
JorgeDev7 <jorgehumbertocolindres@gmail.com>
2023-05-08 10:27:06 -0600

    hook useRouter importado e instanciado en una variable llamada 'router', evento onClick colocado a cada imagen, para realizar un metodo push y redireccionar a un endpoint dinamico

--- src/pages/croissants.js
+++ src/pages/croissants.js
@@ -1,3 +1,5 @@
+import Image from "next/image";
+import { useRouter } from "next/router";
 import { useState, useEffect } from "react"
 import Layout from "@/layout/Layout"
 import axios from "axios"
@@ -6,8 +8,12 @@ export default function Croissants() {
 
     const [croissants, setCroissants] = useState([]);
 
+    const router = useRouter();
+
     const obtenerCroissant = async () => {
-        const { data } = await axios('http://localhost:4000/productos?producto.categoria=croissants');
+        const { data } = (await axios('http://localhost:4000/productos?producto.categoria=croissants', {
+            data: 'croissants'
+        }));
 
         setCroissants(data);
     }
@@ -21,7 +27,17 @@ export default function Croissants() {
         <Layout
             pagina="Croissants"
         >
-            <h1>Croissants</h1>
+            <div className="container px-5 mx-auto grid grid-cols-4 gap-5 mt-10">
+                {croissants?.map(cro => (
+                    <Image
+                        width={300}
+                        height={400}
+                        src={`/assets/images/Croissants/${cro.producto.imagen}.jpg`}
+                        alt={`imagen croissant ${cro.producto.nombre}`}
+                        onClick={() => router.push(`/producto/${cro.producto.id}`)}
+                    />
+                ))}
+            </div>
         </Layout>
     )
 }
