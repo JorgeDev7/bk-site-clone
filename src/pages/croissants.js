7918eec5845a0d9b665af65c98c10bc6baef7850
JorgeDev7 <jorgehumbertocolindres@gmail.com>
2023-05-08 12:05:37 -0600

    Componente Image eliminado, hook useRouter y variable eliminados, la iteracion para mostrar las imagenes de los productos fue reemplazada por componente Producto

--- src/pages/croissants.js
+++ src/pages/croissants.js
@@ -1,12 +1,10 @@
-import Image from "next/image";
-import { useRouter } from "next/router";
 import Layout from "@/layout/Layout"
+import Producto from "@/components/Producto";
 import useBk from "../../hooks/useBk";
 
 export default function Croissants() {
 
     const { croissants } = useBk();
-    const router = useRouter();
 
     return (
         <Layout
@@ -14,14 +12,9 @@ export default function Croissants() {
         >
             <div className="container px-5 mx-auto grid grid-cols-4 gap-5 mt-10">
                 {croissants?.map(cro => (
-                    <Image
+                    <Producto
                         key={cro.producto.id}
-                        width={300}
-                        height={400}
-                        src={`/assets/images/Croissants/${cro.producto.imagen}.jpg`}
-                        alt={`imagen croissant ${cro.producto.nombre}`}
-                        onClick={() => router.push(`/producto/${cro.producto.id}`)}
-                        priority={true}
+                        datos={cro.producto}
                     />
                 ))}
             </div>
