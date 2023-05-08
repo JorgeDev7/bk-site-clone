a01f4d910828a1432c80b96f5613319d5803d4bc
JorgeDev7 <jorgehumbertocolindres@gmail.com>
2023-05-08 10:50:48 -0600

    axios importado, funciones para llamado de api realizadas, variable croissants colocada en area de values para uso en otros archivos

--- context/bkProvider.jsx
+++ context/bkProvider.jsx
@@ -1,12 +1,28 @@
 import { useState, useEffect, createContext } from "react";
+import axios from "axios";
 
 const BkContext = createContext();
 
 const BkProvider = ({ children }) => {
+
+    const [croissants, setCroissants] = useState([]);
+
+    const obtenerCroissant = async () => {
+        const { data } = (await axios('http://localhost:4000/productos?producto.categoria=croissants', {
+            data: 'croissants'
+        }));
+
+        setCroissants(data);
+    }
+
+    useEffect(() => {
+        obtenerCroissant();
+    }, [])
+
     return (
         <BkContext.Provider
             value={{
-
+                croissants
             }}
         >
             {children}
