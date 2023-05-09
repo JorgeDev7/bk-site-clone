9c7bf8412a7687120b85544809795e84e6f833c5
JorgeDev7 <jorgehumbertocolindres@gmail.com>
2023-05-09 11:26:40 -0600

    Endpoint para productos chapin, con hook personalizado se extrae los datos y se itera con el componente producto, variable temporal cambiada para iteracion

--- src/pages/chapin.js
+++ src/pages/chapin.js
@@ -8,7 +8,7 @@ export default function Chapin() {
 
     return (
         <Layout
-            pagina="Croissants"
+            pagina="Chapin"
         >
             <div className="container px-5 mx-auto grid justify-center md:grid-cols-2 lg:grid-cols-4 gap-5 mt-10">
                 {chapin?.map(cha => (
