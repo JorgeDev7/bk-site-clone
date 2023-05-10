c02b39f85b11c50e7ef9b0ef33fb70f7bb83a1b9
JorgeDev7 <jorgehumbertocolindres@gmail.com>
2023-05-10 12:08:08 -0600

    Componente para renderizar los lI del menu de categorias

---
+++ src/components/Categoria.js
@@ -0,0 +1,13 @@
+export default function Categoria({ nombre }) {
+
+    const { categoria } = nombre;
+
+    return (
+        <li className="border-b last-of-type:border-b-0 pb-2 last-of-type:pb-0 pt-2.5 lg:pl-5 lg:pr-10 lg:py-2">
+            <a
+                className={`text-[#666] lg:text-amarillo hover:text-rojo transition-colors font-poppins font-black text-lg uppercase`}
+                href="/croissants"
+            >{categoria}</a>
+        </li>
+    )
+}
