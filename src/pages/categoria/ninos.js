f6fc9990bc377d1159141b1e8574be9a66a33ced
JorgeDev7 <jorgehumbertocolindres@gmail.com>
2023-05-11 11:51:55 -0600

    Endpoint creado y enlazado con sus respectivos datos

---
+++ src/pages/categoria/ninos.js
@@ -0,0 +1,23 @@
+import Layout from "@/layout/Layout"
+import Producto from "@/components/Producto";
+import useBk from "../../../hooks/useBk";
+
+export default function Ninos() {
+
+    const { ninos } = useBk();
+
+    return (
+        <Layout
+            pagina="Chapin"
+        >
+            <div className="container px-5 mx-auto grid justify-center md:grid-cols-2 lg:grid-cols-4 gap-5 mt-10">
+                {ninos?.map(nino => (
+                    <Producto
+                        key={nino.producto.id}
+                        datos={nino.producto}
+                    />
+                ))}
+            </div>
+        </Layout>
+    )
+}
