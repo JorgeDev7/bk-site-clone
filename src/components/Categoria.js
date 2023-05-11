69ddd2d0e473f2b001403512390d55776741e541
JorgeDev7 <jorgehumbertocolindres@gmail.com>
2023-05-10 18:27:32 -0600

    HREF modificado para nuevo endpoint '/categoria/nombre_categoria'

--- src/components/Categoria.js
+++ src/components/Categoria.js
@@ -6,7 +6,7 @@ export default function Categoria({ nombre }) {
         <li className="border-b last-of-type:border-b-0 pb-2 last-of-type:pb-0 pt-2.5 lg:pl-5 lg:pr-10 lg:py-2">
             <a
                 className={`text-[#666] lg:text-amarillo hover:text-rojo transition-colors font-poppins font-black text-lg uppercase`}
-                href={`/${url}`}
+                href={`/categoria/${url}`}
             >{categoria}</a>
         </li>
     )
