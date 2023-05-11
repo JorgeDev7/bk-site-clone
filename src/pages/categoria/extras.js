1f46b3159db4afbc96a9e3b7ab50813ba7778d4b
JorgeDev7 <jorgehumbertocolindres@gmail.com>
2023-05-11 11:26:41 -0600

    Endpoint para categoria extras creado

---
+++ src/pages/categoria/extras.js
@@ -0,0 +1,23 @@
+import Layout from "@/layout/Layout"
+import Producto from "@/components/Producto";
+import useBk from "../../../hooks/useBk";
+
+export default function Extras() {
+
+    const { extras } = useBk();
+
+    return (
+        <Layout
+            pagina="Panqueques"
+        >
+            <div className="container px-5 mx-auto grid justify-center md:grid-cols-2 lg:grid-cols-4 gap-5 mt-10">
+                {extras?.map(ext => (
+                    <Producto
+                        key={ext.producto.id}
+                        datos={ext.producto}
+                    />
+                ))}
+            </div>
+        </Layout>
+    )
+}
