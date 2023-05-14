c7895bdc3c1063d403763e0d02c3a8871342a4f5
JorgeDev7 <jorgehumbertocolindres@gmail.com>
2023-05-13 22:14:50 -0600

    prop 'pagina' del Layout modificado con su respectivo nombre

--- src/pages/categoria/bebidas.js
+++ src/pages/categoria/bebidas.js
@@ -8,7 +8,7 @@ export default function Bebidas() {
 
     return (
         <Layout
-            pagina="Chapin"
+            pagina="Bebidas"
         >
             <div className="container px-5 mx-auto grid justify-center md:grid-cols-2 lg:grid-cols-4 gap-5 mt-10">
                 {bebidas?.map(beb => (
