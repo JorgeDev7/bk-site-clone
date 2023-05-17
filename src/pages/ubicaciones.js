36781d5191a3d9f358d3a3b4ebdd6f4eef7c9d0f
JorgeDev7 <jorgehumbertocolindres@gmail.com>
2023-05-17 12:20:29 -0600

    ubicaciones data transportado por medio del hook personalizado, iteracion con renderizacion del componente Ubicacion.js, DIV creado para crear un layout grid responsive

--- src/pages/ubicaciones.js
+++ src/pages/ubicaciones.js
@@ -3,11 +3,21 @@ import Ubicacion from "@/components/Ubicacion"
 import useBk from "../../hooks/useBk"
 
 export default function ubicaciones() {
+
+    const { ubicaciones } = useBk();
+
     return (
         <Layout
             pagina="Ubicaciones"
         >
-            <Ubicacion />
+            <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
+                {ubicaciones?.map(ubicacion => (
+                    <Ubicacion
+                        key={ubicacion.id}
+                        datos={ubicacion}
+                    />
+                ))}
+            </div>
         </Layout>
     )
 }
