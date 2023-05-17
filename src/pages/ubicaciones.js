6e00b931e69deae4184a484481d9692c8da0291e
JorgeDev7 <jorgehumbertocolindres@gmail.com>
2023-05-17 11:37:27 -0600

    Maquetacion temporal HTML hecha y next/image importado

--- src/pages/ubicaciones.js
+++ src/pages/ubicaciones.js
@@ -1,8 +1,27 @@
+import Image from "next/image"
 import Layout from "@/layout/Layout"
 import useBk from "../../hooks/useBk"
 
 export default function ubicaciones() {
     return (
-        <div>ubicaciones</div>
+        <Layout
+            pagina="Ubicaciones"
+        >
+            <div>
+                <div>
+
+                </div>
+                <div>
+                    <p><span>Direccion:</span>{' '}cdad. de Guatemala</p>
+                    <div>
+                        <p>Horarios:</p>
+                        <ul>
+                            <li>Viernes: <span>12:00</span> - <span>23:00</span></li>
+                        </ul>
+                    </div>
+                    <p><span>Teléfono:</span>{' '}0000-0000</p>
+                </div>
+            </div>
+        </Layout>
     )
 }
