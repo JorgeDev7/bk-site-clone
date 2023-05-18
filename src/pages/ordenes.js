8bc5d3851dc6ce86509430472a2cadb6da8e3503
JorgeDev7 <jorgehumbertocolindres@gmail.com>
2023-05-18 10:15:25 -0600

    Componente Preview.js importado e implementado

--- src/pages/ordenes.js
+++ src/pages/ordenes.js
@@ -1,11 +1,12 @@
 import Layout from "@/layout/Layout"
+import Preview from "@/components/Preview"
 
 export default function ordenes() {
     return (
         <Layout
             pagina='Mis Órdenes'
         >
-
+            <Preview />
         </Layout>
     )
 }
