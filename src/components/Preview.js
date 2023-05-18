9d19eeafe780d42f7d778e869be66b723789fb55
JorgeDev7 <jorgehumbertocolindres@gmail.com>
2023-05-18 10:29:56 -0600

    Estilos creados, props creados para el componente, 'titulo' y 'texto' estos funcionaran para dar un dinamismo al componente al momento de utilizarse y mostrar diversos textos

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,7 +1,24 @@
 import Image from "next/image"
 
-export default function Preview() {
+export default function Preview({ titulo, texto }) {
     return (
-        <div>Preview</div>
+        <div className="flex flex-col items-center gap-5 mt-32 bg-white mx-auto w-[500px] rounded-sm">
+            <div className="bg-white border-8 border-fondo rounded-full -mt-20">
+                <Image
+                    width={140}
+                    height={140}
+                    src='/assets/images/bk-logo.png'
+                    alt="BK logo"
+                    className="p-5 object-cover"
+                />
+            </div>
+            <h2 className="text-amarillo text-3xl uppercase">{titulo}</h2>
+            <p className="text-grisOscuro text-lg">{texto}</p>
+
+            <button
+                type="button"
+                className="bg-amarillo w-full py-5 text-white uppercase hover:bg-amber-500 transition-colors rounded-b-sm"
+            >Ingresar</button>
+        </div>
     )
 }
