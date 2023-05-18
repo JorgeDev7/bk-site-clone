a1af0d3ca487195212027ab6ab7ba8ffe6c743ae
JorgeDev7 <jorgehumbertocolindres@gmail.com>
2023-05-18 09:51:58 -0600

    Endpoint nuevo creado, Layout importado e implementado, importado next/image y next/link

---
+++ src/pages/nuevo.js
@@ -0,0 +1,13 @@
+import Layout from "@/layout/Layout"
+import Image from "next/image"
+import Link from "next/link"
+
+export default function nuevo() {
+    return (
+        <Layout
+            pagina="Lo Nuevo"
+        >
+
+        </Layout>
+    )
+}
