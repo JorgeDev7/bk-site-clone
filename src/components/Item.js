b90cb2095fcb9bad9696aa8b8be42bede7ba0c98
JorgeDev7 <jorgehumbertocolindres@gmail.com>
2023-05-19 10:17:48 -0600

    extras extraidos del provider mediante el hook personalizado, iteracion y retorno de imagen con su respectiva informacion

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,7 +1,17 @@
 import useBk from '../../hooks/useBk'
 
 export default function Item() {
+
+    const { extras } = useBk();
+
     return (
-        <div>Item</div>
+        <div className="carousel-item flex gap-8">
+            {extras?.map(extra => (
+                <img
+                    src={`/assets/images/${extra.producto.categoria}/${extra.producto.imagen}.webp`}
+                    className="w-80"
+                    alt={`imagen producto ${extra.producto.nombre}`} />
+            ))}
+        </div>
     )
 }
