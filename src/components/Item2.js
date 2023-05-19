bde3d967116232891d536dea9349c7778dd918d1
JorgeDev7 <jorgehumbertocolindres@gmail.com>
2023-05-19 10:45:29 -0600

    div añadido para contener imagen y boton de añadir a carrito

--- src/components/Item2.js
+++ src/components/Item2.js
@@ -7,10 +7,19 @@ export default function Item2() {
     return (
         <div className="carousel-item flex gap-8">
             {bebidas?.map(beb => (
-                <img
-                    src={`/assets/images/${beb.producto.categoria}/${beb.producto.imagen}.webp`}
-                    className="w-80"
-                    alt={`imagen producto ${beb.producto.nombre}`} />
+                <div
+                    key={beb.producto.id}
+                    className='flex flex-col justify-center'
+                >
+                    <img
+                        src={`/assets/images/${beb.producto.categoria}/${beb.producto.imagen}.webp`}
+                        className="w-80"
+                        alt={`imagen producto ${beb.producto.nombre}`} />
+                    <button
+                        type="button"
+                        className=" bg-rojo hover:bg-ambar-500 transition-colors text-white py-2 px-4 w-max mx-auto mt-5 xl:w-auto rounded"
+                    ><i className='bx bxs-shopping-bag' ></i> Agregar al carrito</button>
+                </div>
             ))}
         </div>
     )
